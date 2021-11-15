143d561fabec887793c1e784e6214a0e4390813e
ZhaoXinZhang <roger88119@gmail.com>
2021-11-16 00:30:52 +0800

    1116

--- HW8/Exercise4.c
+++ HW8/Exercise4.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 void main(){
     int input=0,input2=0;
-    int arr[10]={0,1,2,3,4,5,6,7,8,9};
     int arr2[10]={11,11,11,11,11,11,11,11,11,11};
-    // printf("%c",arr[0]);
-    // printf("■ ■ ■ ■ ■ ■ ■ ■ ■ ■")
     while(1){
         for (int i = 0; i < 10; i++)
         {
--- HW8/Exercise6.c
+++ HW8/Exercise6.c
@@ -1,11 +1,33 @@
 #include<stdio.h>
-void main(){
-    int a,b=0;
-    printf("請輸入轉換前進制:")
-    scanf("%d",a);
-    printf("請輸數字:")
-    printf("請輸入轉換後進制:")
-    scanf("%d",b);
-    printf()
+int main(){
+    char table[17]="0123456789ABCDEF";
+    int a,b;
+    char s[33];
+    printf("請輸入轉換前進制:");
+    scanf("%d",&a);
+    printf("請輸數字:");
+    scanf("%s",s,sizeof(s));
+    printf("請輸入轉換後進制:");
+    scanf("%d",&b);
+    int num=0;
+    for(int i =0 ;s[i]!='\0';i++){
+        if(s[i]>='0'&&s[i]<='9'){
+            num=num*a+s[i]-'0';
+        }else{
+            num=num*a+s[i]-'A'+10;
+        }
+    }
+    int digit[32],cnt=0;
+    do{
+        digit[cnt++]=num%b;
+        printf("%d/%d  %d\n",num,b,num%b);
+        num=num/b;
+        
+    }
+    while (num!=0);
+    for(int i=cnt-1;i>=0;i--){
+        printf("%c",table[digit[i]]);
+    }
+    printf("\n");
     
 }
